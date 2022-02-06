699d176421db704ab50ce3f600058d5e04619574
SpiffTrip <95441222+SpiffTrip@users.noreply.github.com>
2022-02-06 16:08:22 -0700

    attempted to append data to application, ran out of time

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,45 @@ var inputEl = document.getElementById("city-name-input")
 
 var buttonEL = document.getElementById("btn")
 
-buttonEL.addEventListener("click", function(event){
+buttonEL.addEventListener("click", function (event) {
     event.preventDefault()
-   var city = inputEl.value 
-   console.log(city);
-   getLocation(city);
+    var city = inputEl.value
+    console.log(city);
+    getLocation(city);
 })
 
-function getLocation(city){
+function getLocation(city) {
     var URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4bbb93c538815e4e4cd4fc3a63e21c97`
     fetch(URL)
-    .then(function(result){
-        return result.json()
-    }).then( function(apiresults){
-        console.log(apiresults)
-        var lat =apiresults.coord.lat
-        var long =apiresults.coord.lon
-        onecallApi(lat,long)
-    })
+        .then(function (result) {
+            return result.json()
+        }).then(function (apiresults) {
+            console.log(apiresults)
+            var lat = apiresults.coord.lat
+            var long = apiresults.coord.lon
+            onecallApi(lat, long) 
+            
+            var wind = apiresults.wind
+            var infoDiv = document.getElementById("info")
+            var pInfo = document.createElement("P")
+
+            pInfo.textContent = wind
+            infoDiv.appendChild(pInfo);
+        })
 }
 
 
-function onecallApi(lat,long) {
+function onecallApi(lat, long) {
     var URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,daily&appid=4bbb93c538815e4e4cd4fc3a63e21c97&units=imperial`
     fetch(URL)
-    .then(function(result){
-        return result.json()
-    }).then( function(apiresults){
-        console.log(apiresults)
-    })
+        .then(function (result) {
+            return result.json()
+        }).then(function (apiresults) {
+            console.log(apiresults)
+         
+        })
+       
+           
+
 }
 
